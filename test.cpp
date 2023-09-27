ad42b313153cf3bab5d55154183da333c8bd3aa2
Foyez Ahmed <fattahahmed506988@gmail.com>
2023-09-27 22:40:51 +0600

    Updated the code

--- test.cpp
+++ test.cpp
@@ -3,42 +3,66 @@
 #include <sstream>
 #include <vector>
 #include <set>
+#include <algorithm>
+#include <cctype>
 using namespace std;
+
 // Set of C keywords
 const std::set<string> keywords = {
-    "auto", "break", "case", "char", "const", "continue", "default", "do",
-    "double", "else", "enum", "extern", "float", "for", "goto", "if", "int",
-    "long", "register", "return", "short", "signed", "sizeof", "static", "struct",
-    "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+  "auto", "break", "case", "char", "const", "continue", "default", "do",
+  "double", "else", "enum", "extern", "float", "for", "goto", "if", "int",
+  "long", "register", "return", "short", "signed", "sizeof", "static", "struct",
+  "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
 };
+
+// Function to remove special characters from a string
+string removeSpecialCharacters(const string& str) {
+  string result;
+  bool lastWasAlphanumeric = false; // Keep track of whether the last character was alphanumeric
+
+  for (char c : str) {
+    if (isalnum(c)) {
+      result += c;
+      lastWasAlphanumeric = true;
+    } else if (c == '_' && lastWasAlphanumeric) {
+      result += c; // Allow underscores in the middle of words
+    } else {
+      lastWasAlphanumeric = false;
+    }
+  }
+  return result;
+}
 
 // Function to tokenize a C statement
 vector<string> tokenizeCStatement(const string& statement) {
-    vector<string> tokens;
-    istringstream stream(statement);
-    string token;
+  vector<string> tokens;
+  istringstream stream(statement);
+  string token;
 
-    while (stream >> token) {
-        // Check if the token is a keyword
-        if (keywords.count(token)) {
-            tokens.push_back("Keyword: " + token);
-        }
+  while (stream >> token) {
+    // Remove special characters from the token
+    string cleanToken = removeSpecialCharacters(token);
+
+    // Check if the token (after removing special characters) is a keyword
+    if (keywords.count(cleanToken)) {
+      tokens.push_back("Keyword: " + cleanToken);
     }
+  }
 
-    return tokens;
+  return tokens;
 }
 
 int main() {
-    string input;
-    cout << "Enter a C statement: ";
-    getline(cin, input);
+  string input;
+  cout << "Enter a C statement: ";
+  getline(cin, input);
 
-    vector<string> tokens = tokenizeCStatement(input);
+  vector<string> tokens = tokenizeCStatement(input);
 
-    cout << "Tokens:" << endl;
-    for (const string& token : tokens) {
-        cout << token << endl;
-    }
+  cout << "Tokens:" << endl;
+  for (const string& token : tokens) {
+    cout << token << endl;
+  }
 
-    return 0;
+  return 0;
 }
